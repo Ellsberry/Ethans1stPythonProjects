713c6ac7a2d011530f8d4b3858ba8bc410614d29
ellsberry <sellsberry@sbcglobal.net>
2023-06-07 14:54:55 -0400

    Initial Commit

---
+++ 1stProject.py
@@ -0,0 +1,24 @@
+"""This is the beginning of using the IDE,
+    Integrated Development Environment,
+    to develop a Python program
+
+    USing 3 quotes at the beginning of a program creates a DocString description
+
+    The 3 quotes is a way of commenting multiple lines while
+    we use the # to comment on one line
+"""
+
+# import turtle and turtles Screen package
+
+
+# Print a message on the screen
+
+
+# Add a = 25 to b = 200 giving c and print all variables
+
+
+# Show a turtle screen object with a turtle on it
+
+
+# Move the turtle on the screen using a arrow key
+
